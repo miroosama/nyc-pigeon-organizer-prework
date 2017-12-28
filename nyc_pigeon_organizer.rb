b0e803d080df5713befde25eef3b9ea480aa67e7
osama miro <o.miro650@gmail.com>
2017-12-28 03:38:45 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  my_hash = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+      array.each do |str|
+        if !my_hash.has_key?(str)
+          my_hash[str] = {}
+        end
+        if !my_hash[str].has_key?(property)
+          my_hash[str][property] = []
+        end
+if !my_hash[str][property].include?(attribute)
+          my_hash[str][property] << attribute.to_s
+        end
+      end
+   end
+end
+my_hash
 end
